1775888b0212d1c9b9aba4b71f9d8f9e4a499e7c
cyril-muzikaloid <cyril.makloufi@gmail.com>
2025-04-04 12:27:53 +0200

    New systems and tkinter

--- src/TKDisplay.py
+++ src/TKDisplay.py
@@ -3,20 +3,31 @@ from Setting import *
 from tkinter import *
 from tkinter import ttk
 
+dict_var = {}
+
 class TKElement(IElement):
-    def __init__(self, value, type, style = None, styleName = "", index = 0):
-        IElement.__init__(self, value)
+    def __init__(self, element, type, variable:Variable, style = None, styleName = "", index = 0):
+        IElement.__init__(self)
         self.__type = type
         self.__index = index
         self.__style:ttk.Style = style
         self.__styleName:str = styleName
+        self.__element = element
+        self.__variable:Variable = variable
+        print(f"TKElement créé avec variable: {self.__variable.get()}")
 
     def SetBGColor(self, color):
         if self.__style and self.__styleName != "":
             self.__style.configure(self.__styleName, background=color)
 
     def UpdateValue(self, value):
-        print("Update Value")
+        self.__variable.set(value)
+
+    def GetValue(self):
+        return self.__variable.get()
+    
+    def GetElement(self):
+        return self.__element
 
 class TKDisplay(IDisplay):
     def __init__(self) -> None:
@@ -24,10 +35,10 @@ class TKDisplay(IDisplay):
 
         self.__root = Tk()
 
-        self.__root.tk.call('source', 'Forest-ttk-theme-master/forest-dark.tcl')
+        #self.__root.tk.call('source', 'Forest-ttk-theme-master/forest-dark.tcl')
 
         self.__style = ttk.Style(self.__root)
-        self.__style.theme_use('forest-dark')
+        #self.__style.theme_use('forest-dark')
 
         self.__root.title("Settingator")
 
@@ -37,13 +48,11 @@ class TKDisplay(IDisplay):
         #self.__root.columnconfigure(0, weight=1)
         #self.__root.rowconfigure(0, weight=1)
 
-        #testBUtton = ttk.Button(self.__mainFrame, text="Test", command=lambda : print("yolo")).grid(column=0, row=0)
-
     def Update(self) -> Setting:
+        self.UpdateLayout()
         self.__root.update()
-        self.UpdateLayout(None)
 
-    def __UpdatePrelayout(self, element:PreLayoutElement, parent, childIndex=0):
+    def __UpdateLayout(self, element:LayoutElement, parent, childIndex=0):
 
         #elementsToRemove = element.GetElementsToRemoveFromView()
         #while elementsToRemove.__len__():
@@ -56,16 +65,17 @@ class TKDisplay(IDisplay):
             type:int = element.GetType()
             name = element.GetName()
             
-            if isinstance(name, Pointer):
+            if isinstance(name, Mutable):
                 name = name.GetValue()
-
-            key = element.GetKey()
-            ret:Pointer = element.GetRet()
                 
             row = 1
             column = 1
 
             newElement:ttk.Widget
+
+            styleName = str(element)
+
+            elementVariable = StringVar(master=self.__root, value=element.GetValue())
 
             if element.GetParent():
                 if element.GetParent().GetType() == IDP_FRAME:
@@ -75,38 +85,42 @@ class TKDisplay(IDisplay):
                     row = childIndex + 1
 
             if type == IDP_BUTTON:
-                newElement = ttk.Button(parent, text=name, command=key)
-                
-                if ret:
-                    ret.SetValue(TKElement(newElement, IDP_BUTTON))
+                newElement = ttk.Button(parent, text=name, command=lambda e=element : e.Call(None))
 
             elif type == IDP_TEXT:
+                newElement = ttk.Label(parent, textvariable=elementVariable)
                 
-                newElement = ttk.Label(parent, text=name)
-                
-                if ret:
-                    ret.SetValue(TKElement(newElement, IDP_TEXT))
-
             elif type == IDP_INPUT:
-                inputVar=StringVar(value=name)
-                newElement = ttk.Entry(parent, textvariable=inputVar)
+                newElement = ttk.Entry(parent, textvariable=elementVariable)
+                newElement.bind("<Return>", lambda event, e=element : e.Call(elementVariable.get()))
+                
+                #print("Valeur par défaut:", inputVar.get())
 
-                if ret:
-                    ret.SetValue(TKElement(newElement, IDP_INPUT))
+                #if ret:
+                #    ret.SetValue(TKElement(newElement, IDP_INPUT))
+
+            elif type == IDP_CHECK:
+                newElement = ttk.Checkbutton(parent, text=name, textvariable=elementVariable, command=lambda e=element : e.Call(elementVariable.get()))
+                
+                #if element.GetValue() == True:
+                #    newElement.state(['selected'])
+                #else:
+                #    newElement.state(['!selected'])
+
+    
 
             elif type == IDP_COLUMN:
-
-                styleName = str(element)+".TFrame"
-                self.__style.configure(styleName)
-                newElement = ttk.Frame(parent, style=styleName)
-
-                if ret:
-                    ret.SetValue(TKElement(newElement, IDP_COLUMN, self.__style, styleName))
-
+                
+                if name == "":
+                    styleName += ".TFrame"
+                    self.__style.configure(styleName)
+                    newElement = ttk.Frame(parent, style=styleName)
+                else:
+                    styleName += ".TLabelframe"
+                    self.__style.configure(styleName)
+                    newElement = ttk.Labelframe(parent, text=name, style=styleName)
 
             elif type == IDP_FRAME:
-
-                styleName =str(element)
 
                 if name == '':
                     newElement = ttk.Frame(parent)
@@ -114,36 +128,35 @@ class TKDisplay(IDisplay):
                     styleName += ".TLabelframe"
                     self.__style.configure(styleName)
                     newElement = ttk.Labelframe(parent, text=name, style=styleName)
-
-                if ret:
-                    ret.SetValue(TKElement(newElement, IDP_FRAME, self.__style, styleName))
  
             newElement.grid(column=column, row=row, sticky=(N, W, E), padx=5, pady=5)
-
+            element.SetIElement(TKElement(newElement, type, elementVariable, self.__style, styleName))
+            dict_var[element] = elementVariable
             self.__UpdateChildLayout(element, newElement)
+
+        else:
+            self.__UpdateChildLayout(element, element.GetIElement().GetElement())
+
 
 
         element.SetModified(False)
 
-    def __UpdateChildLayout(self, parentElement:PreLayoutElement, parent) -> None:
+    def __UpdateChildLayout(self, parentElement:LayoutElement, parent) -> None:
         
-        if isinstance(parentElement.GetKey(), list):
-            childElement:PreLayoutElement
+        if isinstance(parentElement.GetChildren(), list):
+            childElement:LayoutElement
 
             childIndex = 0
-            for childElement in parentElement.GetKey():
+            for childElement in parentElement.GetChildren():
 
                 if childElement.IsModified():
-                    self.__UpdatePrelayout(childElement, parent, childIndex)
+                    self.__UpdateLayout(childElement, parent, childIndex)
                 
                 childIndex += 1
 
-    def UpdateLayout(self, slaveSettings:dict) -> None:
-        if self._PreLayout.IsModified():
-            self.__UpdatePrelayout(self._PreLayout, self.__mainFrame)
-
-        if slaveSettings:
-            pass
+    def UpdateLayout(self) -> None:
+        if self._Layout.IsModified():
+            self.__UpdateLayout(self._Layout, self.__mainFrame)
 
     def UpdateSetting(self, setting:Setting) -> None:
         pass
--- src/testTK.py
+++ src/testTK.py
@@ -1,59 +1,28 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-        s.configure('TButton', foreground='blue')
-    except ValueError:
-        pass
+# Fonction pour mettre à jour la valeur de la StringVar
+def update_label():
+    elementVariable.set("Valeur mise à jour!")
 
-def caca(secondFrame):
-    s.configure('BB.TButton', background='red', foreground='blue', color='green')
-    s.configure('TButton', foreground='red')
-    elem = ttk.Button(secondFrame, text="test4", style="BB.TButton")
-    elem.grid(column=2, row=2, sticky=(N, W, E, S))
-    print(elem['style'])
+# Créer la fenêtre principale
+root = tk.Tk()
+root.title("Exemple StringVar avec Label")
 
-root = Tk()
-root.title("Feet to Meters")
+# Créer une StringVar et un Label associé
+elementVariable = tk.StringVar(value="Valeur initiale")
 
-s = ttk.Style()
+# Créer le Label qui utilise la StringVar
+label = ttk.Label(root, textvariable=elementVariable)
+label.pack(padx=10, pady=10)
 
-s.configure('Test.TLabelframe', background='red')
+# Créer un bouton pour mettre à jour la StringVar
+button = ttk.Button(root, text="Mettre à jour", command=update_label)
+button.pack(padx=10, pady=10)
 
-mainframe = ttk.Frame(root)
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
 
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
 
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-secondFrame = ttk.Labelframe(mainframe)
-secondFrame.grid(column=4, row=4, sticky=(N, W, E, S))
-
-ttk.Button(secondFrame, text="test1").grid(column=1, row=1, sticky=(N, W, E, S))
-ttk.Button(secondFrame, text="test2").grid(column=1, row=2, sticky=(N, W, E, S))
-ttk.Button(secondFrame, text="test3").grid(column=2, row=1, sticky=(N, W, E, S))
-
-caca(secondFrame)
-
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind("<Return>", calculate)
-
-root.mainloop()
+# Démarrer la boucle principale de Tkinter
+#root.mainloop()
+while(1):
+    root.update()
